a1ca474863e77b861658b79fe653e96be8d06433
adi0300 <adiciatlosi@yahoo.com>
2022-03-29 17:03:29 +0300

    Project initialization with React

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+import logocompanie from './logo_companie.jpeg';
+import logosite  from './logo_site.jpeg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className='App-header'>
+        <h1>Home Page AlphaFBase</h1>
+        <h3>Logo companie</h3>
+        <img src={logocompanie} className='App-logo' alt="logocompanie" />
+        <h3>Logo site</h3>
+        <img src={logosite} className='App-logo' alt="logosite" />
       </header>
     </div>
   );
