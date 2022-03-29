81ca9bb25afcccfa3553c517ba26906bd81b1c79
AndreiBalu <andrei.baluta@yahoo.com>
2022-03-29 19:48:10 +0300

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import logocompanie from './logo_companie.jpeg';
-import logosite  from './logo_site.jpeg';
-import './App.css';
+import logocompanie from "./logo_companie.jpeg";
+import logosite from "./logo_site.jpeg";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <header className='App-header'>
+      <header className="App-header">
         <h1>Home Page AlphaFBase</h1>
         <h3>Logo companie</h3>
-        <img src={logocompanie} className='App-logo' alt="logocompanie" />
+        <img src={logocompanie} className="App-logo" alt="logocompanie" />
         <h3>Logo site</h3>
-        <img src={logosite} className='App-logo' alt="logosite" />
+        <img src={logosite} className="App-logo" alt="logosite" />
       </header>
     </div>
   );
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function Footer() {
+  return (
+    <div>
+      <footer className="App-footer">
+        <h1>Sanatate numa bine!</h1>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
