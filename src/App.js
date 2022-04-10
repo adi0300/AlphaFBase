3c6b07fc3d1975893aad36dd480820b17b84839a
adi0300 <adiciatlosi@yahoo.com>
2022-04-10 14:40:22 +0300

    Added Services page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import MainNavBar from "./components/MainNavBar";
 import { Routes } from 'react-router-dom';
 import { Route } from "react-router-dom";
 import Home from "./pages/Home";
-
+import Services from "./pages/Services";
 function App() {
   return (
     <div>
       <MainNavBar />
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/services" element={<Services/>}/>
       </Routes>
       <Footer />
     </div>
