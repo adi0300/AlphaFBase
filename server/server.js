a40a0ae8fe93a9d2197d683cc7d2a1041d4ffe9e
adi0300 <adiciatlosi@yahoo.com>
2022-05-16 21:00:18 +0300

    Connected to the database and created a user model

--- server/server.js
+++ server/server.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(
+"mongodb+srv://User:parolauser@clusteralpha.p4a6i.mongodb.net/alphafbaseapp?retryWrites=true&w=majority"
+)
+.then(() => console.log('Now connected to MongoDB!'))
+.catch(err => console.error('Something went wrong', err));
 
 app.listen(3001, ()=>{
     console.log("First run of the server");
