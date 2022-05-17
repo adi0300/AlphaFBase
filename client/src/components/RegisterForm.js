85604d4aad6834f448f352f152c0a82f8751174b
adi0300 <adiciatlosi@yahoo.com>
2022-05-17 21:53:22 +0300

    Register user now working on frontend through register form

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -1,5 +1,30 @@
 import classes from './RegisterForm.module.css';
+import {useState} from 'react';
+import Axios from 'axios';
+
 function RegisterForm(){
+    const setUserList = useState([]);
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [clubname, setClubname] = useState("");
+    const [password, setPassword] = useState("");
+
+    const registerUser = () =>{
+        Axios.post("http://localhost:3001/api/register", {
+            username,
+            email,
+            clubname,
+            password
+        }).then((response) =>{
+            setUserList([ {
+                username,
+                email,
+                clubname,
+                password
+            }])
+        });
+    };
+
     return(
         <div>
             <form className={classes.form}>
@@ -10,19 +35,27 @@ function RegisterForm(){
                 <div className={classes.clear}></div>
 
                 <div className={classes.div}>
-                    <input type="text" required id="userName" placeholder="Username "/>
+                    <input type="text" required id="userName" placeholder="Username "  onChange={(event) => {
+                  setUsername(event.target.value);
+                 }}/>
                 </div>
                 <div className={classes.div}>
-                    <input type="email" required id="email" placeholder="Email "/>
+                    <input type="email" required id="email" placeholder="Email " onChange={(event) => {
+                  setEmail(event.target.value);
+                 }}/>
                 </div>
 
                 <div className={classes.clear}></div>
 
                 <div className={classes.div}>
-                    <input type="text" required id="clubName" placeholder="Club Name"/>
+                    <input type="text" required id="clubName" placeholder="Club Name" onChange={(event) => {
+                  setClubname(event.target.value);
+                 }}/>
                 </div>
                 <div className={classes.div}>
-                    <input type="text" required id="password" placeholder="Password"/>
+                    <input type="password" required id="password" placeholder="Password" onChange={(event) => {
+                  setPassword(event.target.value);
+                 }}/>
                 </div>
 
                 <div className={classes.clear}></div>
@@ -33,7 +66,7 @@ function RegisterForm(){
                 </div>
 
                 <div className={classes.action}>
-                    <button>SIGN UP</button>
+                    <button onClick={registerUser}>SIGN UP</button>
                 </div>
             </form>
         </div>
