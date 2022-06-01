3cb613eab458d89555fa0769c811cc45ba703d72
adi0300 <adiciatlosi@yahoo.com>
2022-06-01 15:20:03 +0300

    Club name showing dynamically for every user

--- client/src/components/HomeLoggedInDesc.js
+++ client/src/components/HomeLoggedInDesc.js
@@ -1,14 +1,35 @@
 import classes from "./HomeLoggedInDesc.module.css";
+import {useState, useEffect} from 'react';
+import Axios from 'axios';
 
 function HomeLoggedInDesc(props) {
+  const [clubList, setClubList] = useState([]);
+  const username = localStorage.getItem("username");
+
+  useEffect( () => {
+    Axios.get("http://localhost:3001/api/get/clublisthome")
+    .then( (res) => {
+      setClubList(res.data);
+    })
+  }, []);
+
   return (
     <div>
       <div className={classes.titluclub}>
-        <h1>{props.title}</h1>
+        { clubList.filter(club => club.username === username)
+                         .map((club) => {
+            return(
+              <h1>{club.clubname}</h1>
+            )
+         })}
       </div>
       <div className={classes.chestii}>
         <div className={classes.paragraph}>
-          <p>{props.paragraph}</p>
+          <h6>Founded in: 2018</h6>
+          <h6>Location: Barsana</h6>
+          <h6>Current season: 2021-2022</h6>
+          <h6>League table position: 10</h6>
+          <h6>Manager: Dorinel Munteanu</h6>
         </div>
 
         <div >
